832194d5ce3f13d5abd5f5d89223e079fc013c10
Michael Sohnen <michaelsohnenacademic@gmail.com>
2026-01-04 18:44:12 -0500

    Progress.

--- examples/mr_sandman_12edo_like_JI.py
+++ examples/mr_sandman_12edo_like_JI.py
@@ -65,55 +65,70 @@ def write_notes(track: EventScheduler, start: float, notes):
 # ============================================================
 
 BASSLINE_LINES = """
-#
+# intro bar 1
 C.3.e*6 C.3.e*2
 F.3.e*4 G.2.e*4
 
-#
+# intro bar 2
 C.3.e*2 C.3.e*2
 C.3.e*2 C.3.e*2
 F.3.e*2 F.3.e*2
 G.2.e*2 G.2.e*2
 
-#
+# Mr Sandman
 C.3.e G.3.e E.3.e G.3.e
 C.3.e G.3.e E.3.e G.3.e
+
+# Give me a dream
 C#.3.e A.3.e E.3.e A.3.e
 A.2.e E.3.e G.3.e E.3.e
 
+# Make him the cutest that I've ever seen
 D.3.e F.3.e A.3.e F.3.e
 D.3.e F.3.e A.3.e F.3.e
 G.2.e B.2.e D.3.e F.3.e A.3.e F.3.e D.3.e G.2.e
+
+# Give him two lips
+C.3.e E.3.e G.3.e E.3.e
+A.2.e E.3.e C.3.e E.3.e
+
+# like roses and clovers
+G.2.e B.2.e D.3.e
+C.3.e E.3.e C.3.e G.2.e E.2.e
 """.splitlines()
 
 MELODY_LINES = """
-#
+# Intro bar 1
 C.4.e E.4.e G.4.e B.4.e
 A.4.e G.4.e E.4.e C.4.e
 D.4.e F.4.e A.4.e C.5.e
 B.4.e*4
 
-#
+# Intro bar 2
 C.4.e E.4.e G.4.e B.4.e
 A.4.e G.4.e E.4.e C.4.e
 D.4.e F.4.e A.4.e C.5.e
 B.4.e*2 A.4.e B.4.e
 
-#
+# Mr sandman 
 B.4.q
 A.4.q*3
+
+# Give me a dream
 B.4.e B.4.e A.4.e B.4.e+e*4
 
-#
+# Make him the cutest that I've ever seen
 R.e
 C.5.e C.5.e B.4.e C.5.e B.4.e
 R.e B.4.e
-F.4.e*2 E.4.e F.4.e+q*2
+F.4.e E.4.e E.4.e F.4.e+q*2
 
-R.e
+# Give him two lips
 R.e
 A.4.e A.4.e G.4.e A.4.e
 R.e*2
+
+# Like roses and clovers
 G.4.e E.4.e E.4.e D.4.e
 E.4.q G.4.q+e
 """.splitlines()
@@ -133,7 +148,7 @@ for line in MELODY_LINES:
 frames1 = track1.render_collect()
 frames2 = track2.render_collect()
 
-frames = mix(((10 ** (-3 / 20), frames1), (10 ** (-30 / 20), frames2)))
+frames = mix(((10 ** (-3 / 20), frames1), (10 ** (-28.5 / 20), frames2)))
 
 
 print("Playing...")
